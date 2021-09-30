1691b23a86ded9e9c6120c55ebd4ebdb9479840a
vcastilh <viniciaofelix@gmail.com>
2021-09-30 08:48:09 -0300

    Fixed memory leaks and segfault
    Handled lines with no '\n'
    Handled wild pointers after free
    Instead of a loop, it's using the return from ft_strchr() to hold '\n'
    address.

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,43 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 20:05:38 by vcastilh          #+#    #+#             */
-/*   Updated: 2021/09/28 15:45:34 by vcastilh         ###   ########.fr       */
+/*   Updated: 2021/09/29 16:21:14 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*get_line(char	**buffer_backup)
+static char	*get_line(char	**buffer_backup, char	*ptr_n)
 {
-	size_t	i;
 	char	*tmp_free;
 	char	*line_return;
-
-	i = 0;
-	while ((*buffer_backup)[i] != '\n' && (*buffer_backup)[i] != '\0')
-		i++;
+	int	i;
+	//while ((*buffer_backup)[i] != '\n' && (*buffer_backup)[i] != '\0')
+	//	i++;
+	/*if (*buffer_backup[i] == 0)
+	{
+		line_return = *buffer_backup;
+		*buffer_backup = NULL;
+		return (line_return);
+	}*/
+	if (!ptr_n)
+	{
+		if (**buffer_backup == '\0')
+		{
+			free(*buffer_backup);
+			*buffer_backup = NULL;
+			return (NULL);
+		}
+		line_return = *buffer_backup;
+		*buffer_backup = NULL;
+		return (line_return);
+	}
 	tmp_free = *buffer_backup;
-	line_return = ft_substr(tmp_free, 0, i + 1);
-	*buffer_backup = ft_strdup(&tmp_free[i + 1]);
+	i =  ptr_n - tmp_free + 1;
+	line_return = ft_substr(tmp_free, 0, i);
+	*buffer_backup = ft_strdup(&tmp_free[i]);
 	free(tmp_free);
+	tmp_free = NULL;
 	return (line_return);
 }
 
@@ -35,6 +53,7 @@ char	*get_next_line(int fd)
 	int		bytes_read;
 	char	*tmp_free;
 	char	*line;
+	char	*ptr_nl;
 	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -43,18 +62,23 @@ char	*get_next_line(int fd)
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	bytes_read = 1;
-	while (bytes_read > 0 && !ft_strchr(buffer_backup, '\n'))
+	while (!(ptr_nl = ft_strchr(buffer_backup, '\n')))
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1 || bytes_read == 0)
-			return (NULL);
-		buffer[BUFFER_SIZE] = '\0';
+		{
+			free(buffer);
+			buffer = NULL;
+			return (get_line(&buffer_backup, 0));
+		}
+		buffer[bytes_read] = '\0';
 		tmp_free = buffer_backup;
 		buffer_backup = ft_strjoin(tmp_free, buffer);
 		free(tmp_free);
+		tmp_free = NULL;
 	}
-	line = get_line(&buffer_backup);
+	line = get_line(&buffer_backup, ptr_nl);
 	free(buffer);
+	buffer = NULL;
 	return (line);
 }
